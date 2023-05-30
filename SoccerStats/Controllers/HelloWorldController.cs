28bf43dcf9ac6a01aca380fa67c0c582b763d14a
adamdeere <adam.deere@hotmail.com>
2023-05-30 20:56:18 +0100

    following along with a tutorial to get started

---
+++ SoccerStats/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoccerStats.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Hello human!";
+        }
+    }
+}
