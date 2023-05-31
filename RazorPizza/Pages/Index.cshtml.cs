34afb821083992e762d76aaf0b7549a8474b6003
adamdeere <adam.deere@hotmail.com>
2023-05-31 20:47:43 +0100

    pizza update done

--- RazorPizza/Pages/Index.cshtml.cs
+++ RazorPizza/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizza.Pages
 {
@@ -14,7 +13,6 @@ namespace RazorPizza.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
