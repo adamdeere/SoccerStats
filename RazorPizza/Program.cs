ee0c8a28e9a3a68d9dfe9672a34f923df7d319ad
adamdeere <adam.deere@hotmail.com>
2023-06-01 00:25:11 +0100

    Connected to a database on MySql server manager

--- RazorPizza/Program.cs
+++ RazorPizza/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPizza.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Add services to the container.
 builder.Services.AddRazorPages();
 
