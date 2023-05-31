04d0aa47eafa20566e2ff8ca33cf791fafa0f523
adamdeere <adam.deere@hotmail.com>
2023-05-31 21:28:37 +0100

    it will do

--- RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Models;
 
 namespace RazorPizza.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel? Pizza { get; set; }
         public void OnGet()
         {
         }
