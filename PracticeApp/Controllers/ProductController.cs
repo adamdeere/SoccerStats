e836b7f45ee5cabad4ace266b3a998fa37e2cd6f
adamdeere <adam.deere@hotmail.com>
2023-06-21 10:48:09 +0100

    lol

---
+++ PracticeApp/Controllers/ProductController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PracticeApp.Data;
+using PracticeApp.Models;
+
+namespace PracticeApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly PracticeAppDbContext _context;
+
+        public ProductController(PracticeAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            return _context.ProductModel != null ?
+                        View(await _context.ProductModel.ToListAsync()) :
+                        Problem("Entity set 'PracticeAppDbContext.ProductModel'  is null.");
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.ProductModel == null)
+            {
+                return NotFound();
+            }
+
+            var productModel = await _context.ProductModel
+                .FirstOrDefaultAsync(m => m.SKUCode == id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(productModel);
+        }
+
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SKUCode,Description,Length,Height,Width,Weight")] ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productModel);
+        }
+
+        // GET: Product/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.ProductModel == null)
+            {
+                return NotFound();
+            }
+
+            var productModel = await _context.ProductModel.FindAsync(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+            return View(productModel);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("SKUCode,Description,Length,Height,Width,Weight")] ProductModel productModel)
+        {
+            if (id != productModel.SKUCode)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductModelExists(productModel.SKUCode))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productModel);
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.ProductModel == null)
+            {
+                return NotFound();
+            }
+
+            var productModel = await _context.ProductModel
+                .FirstOrDefaultAsync(m => m.SKUCode == id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(productModel);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.ProductModel == null)
+            {
+                return Problem("Entity set 'PracticeAppDbContext.ProductModel'  is null.");
+            }
+            var productModel = await _context.ProductModel.FindAsync(id);
+            if (productModel != null)
+            {
+                _context.ProductModel.Remove(productModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductModelExists(string id)
+        {
+            return (_context.ProductModel?.Any(e => e.SKUCode == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PracticeApp/Migrations/20230614130114_InitalCreate.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PracticeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocationModel",
+                columns: table => new
+                {
+                    LocationId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Length = table.Column<float>(type: "real", nullable: false),
+                    Height = table.Column<float>(type: "real", nullable: false),
+                    Width = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationModel", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductModel",
+                columns: table => new
+                {
+                    SKUCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Length = table.Column<float>(type: "real", nullable: false),
+                    Height = table.Column<float>(type: "real", nullable: false),
+                    Width = table.Column<float>(type: "real", nullable: false),
+                    Weight = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductModel", x => x.SKUCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceiptModel",
+                columns: table => new
+                {
+                    GRN = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Arrival = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpectedArrival = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalWeight = table.Column<float>(type: "real", nullable: false),
+                    TotalCube = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptModel", x => x.GRN);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemModel",
+                columns: table => new
+                {
+                    ItemNo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GRN = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<float>(type: "real", nullable: false),
+                    SKUCode = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemModel", x => x.ItemNo);
+                    table.ForeignKey(
+                        name: "FK_ItemModel_ProductModel_SKUCode",
+                        column: x => x.SKUCode,
+                        principalTable: "ProductModel",
+                        principalColumn: "SKUCode",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemModel_ReceiptModel_GRN",
+                        column: x => x.GRN,
+                        principalTable: "ReceiptModel",
+                        principalColumn: "GRN",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemLocationModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocationId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GRN = table.Column<int>(type: "int", nullable: false),
+                    ItemNo = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemLocationModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemLocationModel_ItemModel_ItemNo",
+                        column: x => x.ItemNo,
+                        principalTable: "ItemModel",
+                        principalColumn: "ItemNo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemLocationModel_LocationModel_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "LocationModel",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemLocationModel_ReceiptModel_GRN",
+                        column: x => x.GRN,
+                        principalTable: "ReceiptModel",
+                        principalColumn: "GRN",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemLocationModel_GRN",
+                table: "ItemLocationModel",
+                column: "GRN");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemLocationModel_ItemNo",
+                table: "ItemLocationModel",
+                column: "ItemNo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemLocationModel_LocationId",
+                table: "ItemLocationModel",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemModel_GRN",
+                table: "ItemModel",
+                column: "GRN");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemModel_SKUCode",
+                table: "ItemModel",
+                column: "SKUCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemLocationModel");
+
+            migrationBuilder.DropTable(
+                name: "ItemModel");
+
+            migrationBuilder.DropTable(
+                name: "LocationModel");
+
+            migrationBuilder.DropTable(
+                name: "ProductModel");
+
+            migrationBuilder.DropTable(
+                name: "ReceiptModel");
+        }
+    }
+}
