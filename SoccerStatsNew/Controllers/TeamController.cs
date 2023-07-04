ab060e6d51590e18d514de0e695d37206f9badd1
adamdeere <adam.deere@hotmail.com>
2023-07-04 11:56:06 +0100

    Can now select teams in a particular league

---
+++ SoccerStatsNew/Controllers/TeamController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerStatsData;
+using SoccerStatsNew.DbServices;
+using SoccerStatsNew.Models;
+using UtilityLibraries;
+
+namespace SoccerStatsNew.Controllers
+{
+    public class TeamController : Controller
+    {
+       
+        private readonly SeasonDbService _seasonDbService;
+        public TeamController(SeasonDbService seasonDbService)
+        {
+            _seasonDbService = seasonDbService;
+        }
+        public async Task<IActionResult> Index(int? league)
+        {
+            TeamPageDisplay? team = null;
+            if (league != null) 
+            {
+                team = await _seasonDbService.GetTeamsDisplayPage((int)league);
+            }
+            return team != null 
+                ? View(team) 
+                : NotFound();
+        }
+    }
+}
---
+++ SoccerStatsNew/Models/TeamPageDisplay.cs
@@ -0,0 +1,11 @@
+﻿using SoccerStatsData;
+
+namespace SoccerStatsNew.Models
+{
+    public class TeamPageDisplay
+    {
+        public List<TeamsPage>? TeamsModelList { get; set; } = new List<TeamsPage>();
+        public List<TeamVenue>? VenueModelList { get; set; } = new List<TeamVenue>();
+        public IEnumerable<SeasonModel>? SeasonModelList { get; set; }
+    }
+}
