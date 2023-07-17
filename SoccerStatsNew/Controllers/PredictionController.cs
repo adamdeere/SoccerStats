91bd552334c3ce00b19e8672bac0b7c8e3ed85cf
adamdeere <adam.deere@hotmail.com>
2023-07-17 16:09:53 +0100

    prediction page filled out with basic styles

--- SoccerStatsNew/Controllers/PredictionController.cs
+++ SoccerStatsNew/Controllers/PredictionController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
 using SoccerStatsData.RequestModels.PredictionRequestFiles;
+using System.Drawing.Text;
 using UtilityLibraries;
 
 namespace SoccerStatsNew.Controllers
 {
     public class PredictionController : Controller
     {
+        private readonly WebService _webService;
+        public PredictionController(WebService webService)
+        {
+            _webService = webService;     
+        }
         public IActionResult Index(int? fixture)
         {
             string urlParams = $"predictions?fixture={fixture}";
@@ -15,6 +23,18 @@ namespace SoccerStatsNew.Controllers
             return predictions != null
                 ? View(predictions.Response[0])
                 : NotFound();
+        }
+
+        public IActionResult Last_Five_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var predictions = JsonHelper.GetObjectFromJsonFile<PredictionRoot>("Test/predictions.json");
+
+            if (predictions != null)
+            {
+                return Json(predictions.Response[0].HeadToHeadStats.ToDataSourceResult(request));
+            }
+
+            return Json(null);
         }
     }
 }
