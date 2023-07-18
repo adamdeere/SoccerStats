5d7bc46e68d0e3aa16f5be81d88437973e572ad3
adamdeere <adam.deere@hotmail.com>
2023-07-18 16:21:52 +0100

    added controller and view for league table

---
+++ SoccerStatsNew/Controllers/LeagueTableController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerStatsData.RequestModels;
+using UtilityLibraries;
+
+namespace SoccerStatsNew.Controllers
+{
+    public class LeagueTableController : Controller
+    {
+        public IActionResult Index()
+        {
+            LeagueTableRoot? lol = null;
+            try
+            {
+                 lol = JsonHelper.GetObjectFromJsonFile<LeagueTableRoot>("Test/tables.json");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+           
+            return View();
+        }
+    }
+}
